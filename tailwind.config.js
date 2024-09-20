6a7186e80cb131bc621f94e66fc0f0725cd96307
Vanshika Singh <110220449+vanshika-hgnis@users.noreply.github.com>
2024-09-20 10:45:19 +0000

    static files images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ module.exports = {
     './templates/**/*.html'
   ],
   theme: {
+    colors: {
+      'background': '#191919',
+      'primary-ascent': '#8878f5',
+      'secondary-background': '#212021',
+      'text-color': '#D9D9D9',
+      'secondtext-color': '#444444',
+      'secondary-ascent': '#9A8DF6',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    },
     extend: {},
   },
   plugins: [],
