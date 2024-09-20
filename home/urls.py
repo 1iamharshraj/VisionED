b5ca63707b9a60fcee553ceaed83d6ac3e0ecb87
Vanshika Singh <110220449+vanshika-hgnis@users.noreply.github.com>
2024-09-20 16:47:48 +0000

    first page setup , url error corrected

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from VisionED import settings
 from . import views
 
 urlpatterns = [
-    path('', views.LoginView.as_view(), name='home'),
+    path('', views.SignUpView.as_view(), name='home'),
     path('login',views.LoginView.as_view(),name='login'),
     path("student_home",views.EducatorHomeView.as_view(),name='student_home')
 ]
